c1a9c20b5d957f8a2ec94b4c07dacd24fefd83e4
Sonualam <sonualam1599@gmail.com>
2024-01-04 18:01:33 +0530

    added layout + scss mixin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+//optimising performance
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Customers = lazy(() => import("./pages/Customers"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/product" element={<Products />} />
+            <Route path="/admin/customer" element={<Customers />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+
+            {/* charts  */}
+
+            {/*  */}
+
+            {/* apps */}
+
+            {/*  */}
+          </Routes>
+        </Suspense>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
