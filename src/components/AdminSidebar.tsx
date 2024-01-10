c2d29e4819e2f1dc5df6f07dbfd2db0d5ef5f158
Sonualam <sonualam1599@gmail.com>
2024-01-10 11:19:56 +0530

    admin sidebar completed

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,10 +1,21 @@
 import { Link, useLocation, Location } from "react-router-dom";
 
 // icons
-import { RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
+import {
+  RiDashboardFill,
+  RiShoppingBag3Fill,
+  RiCoupon3Fill,
+} from "react-icons/ri";
 import { IoIosPeople } from "react-icons/io";
 import { AiFillFileText } from "react-icons/ai";
 import { IconType } from "react-icons";
+import {
+  FaChartBar,
+  FaChartPie,
+  FaChartLine,
+  FaStopwatch,
+  FaGamepad,
+} from "react-icons/fa";
 
 function AdminSidebar() {
   const location = useLocation();
@@ -12,38 +23,96 @@ function AdminSidebar() {
   return (
     <aside>
       <h2>Logo</h2>
-      <div>
-        <h5>Dashboard</h5>
-        <ul>
-          <Li
-            url="/admin/dashboard"
-            text="Dashboard"
-            Icon={RiDashboardFill}
-            location={location}
-          />
-          <Li
-            url="/admin/product"
-            text="Product"
-            Icon={RiShoppingBag3Fill}
-            location={location}
-          />
-          <Li
-            url="/admin/customer"
-            text="Customer"
-            Icon={IoIosPeople}
-            location={location}
-          />
-          <Li
-            url="/admin/transaction"
-            text="Transaction"
-            Icon={AiFillFileText}
-            location={location}
-          />
-        </ul>
-      </div>
+      <DivOne location={location} />
+      <DivTwo location={location} />
+      <DivThree location={location} />
     </aside>
   );
 }
+
+const DivOne = ({ location }: { location: Location }) => (
+  <div>
+    <h5>Dashboard</h5>
+    <ul>
+      <Li
+        url="/admin/dashboard"
+        text="Dashboard"
+        Icon={RiDashboardFill}
+        location={location}
+      />
+      <Li
+        url="/admin/product"
+        text="Product"
+        Icon={RiShoppingBag3Fill}
+        location={location}
+      />
+      <Li
+        url="/admin/customer"
+        text="Customer"
+        Icon={IoIosPeople}
+        location={location}
+      />
+      <Li
+        url="/admin/transaction"
+        text="Transaction"
+        Icon={AiFillFileText}
+        location={location}
+      />
+    </ul>
+  </div>
+);
+
+const DivTwo = ({ location }: { location: Location }) => (
+  <div>
+    <h5>Charts</h5>
+    <ul>
+      <Li
+        url="/admin/chart/bar"
+        text="Bar"
+        Icon={FaChartBar}
+        location={location}
+      />
+      <Li
+        url="/admin/chart/pie"
+        text="Pie"
+        Icon={FaChartPie}
+        location={location}
+      />
+      <Li
+        url="/admin/chart/line"
+        text="Line"
+        Icon={FaChartLine}
+        location={location}
+      />
+    </ul>
+  </div>
+);
+
+const DivThree = ({ location }: { location: Location }) => (
+  <div>
+    <h5>Apps</h5>
+    <ul>
+      <Li
+        url="/admin/app/stopwatch"
+        text="Stopwatch"
+        Icon={FaStopwatch}
+        location={location}
+      />
+      <Li
+        url="/admin/app/coupon"
+        text="Coupon"
+        Icon={RiCoupon3Fill}
+        location={location}
+      />
+      <Li
+        url="/admin/app/toss"
+        text="Toss"
+        Icon={FaGamepad}
+        location={location}
+      />
+    </ul>
+  </div>
+);
 
 interface LiProps {
   url: string;
