aa942f2fc59f3b5256920b158b847c44c03b7687
Sonualam <sonualam1599@gmail.com>
2024-01-12 19:53:25 +0530

    transaction table completed

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,12 +1,92 @@
+import { ReactElement, useCallback, useState } from "react";
 import AdminSidebar from "../components/AdminSidebar";
+import { Column } from "react-table";
+import TableHOC from "../components/TableHOC";
+import { Link } from "react-router-dom";
+
+interface DataType {
+  user: string;
+  amount: number;
+  discount: number;
+  quantity: number;
+  status: ReactElement;
+  action: ReactElement;
+}
+
+const columns: Column<DataType>[] = [
+  {
+    Header: "User",
+    accessor: "user",
+  },
+  {
+    Header: "Amount",
+    accessor: "amount",
+  },
+  {
+    Header: "Discount",
+    accessor: "discount",
+  },
+  {
+    Header: "Quantity",
+    accessor: "quantity",
+  },
+  {
+    Header: "Status",
+    accessor: "status",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+  },
+];
+
+const arr: DataType[] = [
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="red">Processing</span>,
+    action: <Link to="/admin/transaction/sajkfsd">Manage</Link>,
+  },
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="green">Shipped</span>,
+    action: <Link to="/admin/transaction/sajkfsd">Manage</Link>,
+  },
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="purple">Delivered</span>,
+    action: <Link to="/admin/transaction/sajkfsd">Manage</Link>,
+  },
+];
 
 function Transaction() {
+  const [data] = useState<DataType[]>(arr);
+
+  const Table = useCallback(
+    TableHOC<DataType>(
+      columns,
+      data,
+      "dashboard-product-box",
+      "Transactions",
+      true
+    ),
+    []
+  );
+
   return (
     <div className="admin-container">
       {/* sidebar */}
       <AdminSidebar />
       {/* main  */}
-      <main>maincontent</main>
+      <main>{Table()}</main>
     </div>
   );
 }
